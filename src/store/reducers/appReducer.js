e0982c501c071ea832a181714cc58b2006f049d5
natsupy <15712687+natsupy@users.noreply.github.com>
2022-05-08 08:30:29 +0700

    redux chuyen ngon ngu

--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -9,7 +9,7 @@ const initContentOfConfirmModal = {
 
 const initialState = {
     started: true,
-    language: 'vi',
+    language: 'en',
     systemMenuPath: '/system/user-manage',
     contentOfConfirmModal: {
         ...initContentOfConfirmModal
@@ -31,6 +31,12 @@ const appReducer = (state = initialState, action) => {
                     ...action.contentOfConfirmModal
                 }
             }
+        case actionTypes.CHANGE_LANGUAGE:
+            console.log('check redux: ',action)
+                return {
+                    ...state,
+                    language: action.language,
+                }
         default:
             return state;
     }
